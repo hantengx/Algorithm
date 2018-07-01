7233a05ef8c008efb2afed8571c47440ecd75e95
hunter <hantengx@163.com>
2018-07-01 17:42:58 +0800

    add heap sort

---
+++ Algorithm/HeapSort.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm
+{
+    class HeapSort
+    {
+        /// <summary>
+        /// heap sort
+        /// </summary>
+        /// <param name="vs"></param>
+        public static List<int> Sort(List<int> vs)
+        {
+            //build stack
+            int length = vs.Count;
+            for (int i = length / 2 - 1; i >= 0; i--)
+            {
+                Modify(vs, i, length - 1);
+            }
+
+            //swap end and first
+            for (int i = length - 1; i > 0; i--)
+            {
+                int max = vs[0];
+                vs[0] = vs[i];
+                vs[i] = max;
+
+                //Console.WriteLine("max: " + max);
+                //modify stack
+                Modify(vs, 0, i - 1);
+            }
+
+            return vs;
+        }
+
+        /// <summary>
+        /// swap node check every child node
+        /// </summary>
+        /// <param name="vs"></param>
+        /// <param name="from"></param>
+        public static void Modify(List<int> vs, int from, int end)
+        {
+            //Console.WriteLine("join modify j: " + from);
+            while (2 * from + 1 <= end)
+            {
+                int root = from;
+                int left = 2 * from + 1;
+                int right = left + 1;
+                root = vs[root] < vs[left] ? left : root;
+                if (right <= end)
+                {
+                    root = vs[root] < vs[right] ? right : root;
+                }
+
+                if (root != from)
+                {
+                    //Console.WriteLine("swap " + from + " and " + root);
+                    int newRoot = vs[root];
+                    vs[root] = vs[from];
+                    vs[from] = newRoot;
+                    from = root;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Algorithm
 {
@@ -17,18 +18,40 @@ namespace Algorithm
 
         static void Main(string[] args)
         {
+            //var numbers = new List<int>();
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    numbers.Add(i);
+            //}
+            //Sort.Shuffle(numbers);
+
+            //numbers.ForEach(Print);
+            //Console.WriteLine("");
+
+            ////var result = Sort.MergeSort(numbers, 2);
+            //var result = HeapSort.Sort(numbers);
+            //result.ForEach(Print);
+            TestPerformance(999, 1000);
+        }
+
+        private static void TestPerformance(int numberCount, int times)
+        {
             var numbers = new List<int>();
-            for (int i = 0; i < 20; i++)
+            for (int i = 0; i < numberCount; i++)
             {
                 numbers.Add(i);
             }
-            Sort.Shuffle(numbers);
 
-            numbers.ForEach(Print);
-            Console.WriteLine("");
-
-            var result = Sort.MergeSort(numbers, 2);
-            result.ForEach(Print);
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            for (int i = 0; i < times; i++)
+            {
+                Sort.Shuffle(numbers);
+                var result = HeapSort.Sort(numbers);
+                //Sort.MergeSort(numbers, 4);
+            }
+            stopWatch.Stop();
+            Console.WriteLine("time is: " + stopWatch.Elapsed);
         }
     }
 }
